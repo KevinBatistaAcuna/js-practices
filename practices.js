50c570052b41353fa1977b06ff147aefec04f507
Kevin Batista <kbatista23@gmail.com>
2016-09-23 14:50:38 -0600

    some new basic things

--- practices.js
+++ practices.js
@@ -132,3 +132,95 @@ function secondHighest(arr){
     return array[0];
 }
 
+// Construir un numero random, si este numero es divisible entre 3 solamente devuelve Fizz, si es divisible entre 5 solamente 
+// devuelve Buzz, si es divisible entre 3 y 5 devuelve FizzBuzz, si no es divisible por 3 ni 5 devuelve el numero random.
+
+var randomNumber = Math.round(Math.random()*5);
+
+if(randomNumber%3 === 0 && randomNumber != 0){
+    if(randomNumber%5 === 0){
+        console.log("FizzBuzz");
+    }
+    else {
+        console.log("Fizz");
+    }
+}
+else if(randomNumber%5 === 0 && randomNumber != 0){
+    console.log("Buzz");
+}
+else {
+    console.log(randomNumber);
+}
+
+// Crear numero del 1 al 100 en el que cada numero se evalue, si es divisible por 3 devuelve fizz, si es divisible por 5
+// devuelve buzz, si es divisible por 3 y 5 devuelve fizzbuzz y si no es divisible por ninguno devuelve el numero
+
+for(var i=1;i<=100;i++){
+    if(i%3 === 0 && i != 0){
+        if(i%5 === 0){
+            console.log("FizzBuzz");
+        }
+        else {
+            console.log("Fizz");
+        }
+    }
+    else if(i%5 === 0 && i != 0){
+        console.log("Buzz");
+    }
+    else {
+        console.log(i);
+    }
+}
+
+
+var friends = ["Ingrid","Diego","Maria","Luna","Lucas"];
+
+function greet(){
+    for(var i=0;i<friends.length;i++){
+        console.log("Hi " + friends[i]);
+    }
+}
+
+greet();
+
+
+// Objects Methods and Constructors
+
+var mark = {
+    name: "Mark",
+    tshirtColor: "blue"
+}
+
+var lisa = {
+    name: "Lisa",
+    tshirtColor: "red"
+}
+
+function Friend(name, tshirtColor){
+    this.name = name;
+    this.tshirtColor = tshirtColor;
+}
+
+var deny = new Friend("Deny", "green");
+
+console.log(deny.name)
+
+deny.name = "Denny Robson";
+
+console.log(deny.name);
+
+//////////////////////////////////////////////////////////
+
+function animal (name, sound){
+    this.name = name;
+    this.sound = sound;
+}
+
+var dog = new animal("dog", "wouf");
+var cat = new animal("cat", "miau");
+var bee = new animal("bee", "tzzzzzzzzz");
+
+console.log("The " + dog.name + " sounds like " + dog.sound);
+console.log("The " + cat.name + " sounds like " + cat.sound);
+console.log("The " + bee.name + " sounds like " + bee.sound);
+
